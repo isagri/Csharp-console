408eec7c586da8435c82b7e6853078fa055f497f
isagri <isabelle.grillet6@orange.fr>
2018-06-29 15:24:02 +0200

    application console lignes de transport grenoble

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Message
+    {
+        private string message;
+        private int hourJour;
+        private int hourAprem;
+        private int hourSoir;
+
+        public Message(int hourJour=9, int hourAprem=13, int hourSoir=18)
+        {
+            this.hourJour = hourJour;
+            this.hourAprem = hourAprem;
+            this.hourSoir = hourSoir;
+        }
+
+        public string GetHelloMessage(DateTime date1)
+        {
+            if (date1.DayOfWeek == DayOfWeek.Saturday ||
+                date1.DayOfWeek == DayOfWeek.Sunday ||
+                (date1.DayOfWeek == DayOfWeek.Friday && date1.Hour >= 18) ||
+                (date1.DayOfWeek == DayOfWeek.Monday && date1.Hour < 9))
+            {
+
+                message = "Bon Weekend " + Environment.UserName;
+            }
+            else
+            {
+                if (date1.Hour >= hourJour && date1.Hour < hourAprem)
+                {
+                    message = "Bonjour " + Environment.UserName;
+                }
+                else if (date1.Hour >= hourAprem && date1.Hour < hourSoir)
+                {
+
+                    message = "Bon Après-midi " + Environment.UserName;
+
+                }
+                else
+                {
+                    message = "Bonsoir " + Environment.UserName;
+                }
+            }
+            return this.message;
+
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime date1 = new DateTime(2018, 6, 17, 11, 30, 0);
+            //WriteLine(date1);
+
+            Console.WriteLine("Veuillez saisir une date ou 'exit' pour sortir : ");
+            String entree = Console.ReadLine();
+            DateTime date1;
+            while (entree != "exit")
+            {
+                date1 = DateTime.Parse(entree);
+                WriteLine(date1);
+
+                Message message = new Message(9, 13, 18);
+                //Message message = new Message(8, 12, 19);
+                Console.WriteLine(message.GetHelloMessage(date1));
+
+                Console.WriteLine("Veuillez saisir une date ou 'exit' pour sortir : ");
+                entree = Console.ReadLine();
+            }
+        }
+    }
+}
+
